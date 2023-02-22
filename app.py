fcd5e61d92075c5196c40f9f07a68af212893fd9
Victor Karpov <nilafe4@gmail.com>
2023-02-22 11:38:24 +0500

    Init commit start and remeber Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+
+@app.route("/about")
+def route():
+    return "<h1 style='color: red'>About!!!!!!!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
